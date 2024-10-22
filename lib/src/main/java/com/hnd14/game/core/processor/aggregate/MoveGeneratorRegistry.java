c0601321d655a497575b66b61567c9ac8ad0e41e
hnd14 <dnhoang214@gmail.com>
2024-10-22 11:40:04 +0700

    Update registry

--- lib/src/main/java/com/hnd14/game/core/processor/aggregate/MoveGeneratorRegistry.java
+++ lib/src/main/java/com/hnd14/game/core/processor/aggregate/MoveGeneratorRegistry.java
@@ -1,6 +1,7 @@
 package com.hnd14.game.core.processor.aggregate;
 
 import com.hnd14.game.core.concept.*;
+import com.hnd14.game.core.processor.exceptions.UnknownPieceTypeException;
 import com.hnd14.game.core.processor.singular.MoveGenerator;
 
 import java.util.HashMap;
@@ -10,6 +11,9 @@ import java.util.Map;
 public class MoveGeneratorRegistry {
     private final Map<PieceType, MoveGenerator> moveGenerators = new HashMap<>();
     public List<Move> generateCandidateMoves(Piece piece, Board board) {
+        if (moveGenerators.get(piece.getType()) == null) {
+            throw new UnknownPieceTypeException(String.format("Piece type %s is unknown.", piece.getType().name()));
+        }
         return moveGenerators.get(piece.getType()).getCandidateMoves(piece, board);
     }
     public void registerMoveGenerator(PieceType type, MoveGenerator generator) {
---
+++ lib/src/main/java/com/hnd14/game/core/processor/exceptions/UnknownPieceTypeException.java
@@ -0,0 +1,7 @@
+package com.hnd14.game.core.processor.exceptions;
+
+public class UnknownPieceTypeException extends RuntimeException {
+    public UnknownPieceTypeException(String message) {
+        super(message);
+    }
+}
